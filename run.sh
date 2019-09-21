98378cd17444a9e9ca1122527321b359ebb6d9e2
macide213 <39203769+macide213@users.noreply.github.com>
2019-09-21 16:56:48 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 service nginx start
-threads=$(expr `grep processor /proc/cpuinfo | wc -l` \* 2 - 2)
+threads=$(expr `grep processor /proc/cpuinfo | wc -l` - 2)
 cd /home/api.ai-kaldi-asr-model
 ../asr-server/fcgi-nnet3-decoder --fcgi-endofspeech=false --fcgi-threads-number=$threads --fcgi-socket=:9000
